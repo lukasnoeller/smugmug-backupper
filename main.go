35bb01f385199a7b7e6b16b9043a55bb27692aad
Lukas <lukasnoller@gmail.com>
2026-07-19 19:20:34 +0200

    main.go: Work on oath 1.0a authenitcation

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -17,7 +20,9 @@ func main() {
 		fmt.Println("SMUGMUG_API_KEY and/or SMUGMUG_API_SECRET missing")
 		return
 	}
-	url := "https://api.smugmug.com/api/v2"
+	// Step 1: Get temporary request token
+	fmt.Println("Connecting to smug  mug to receive temporary request token...")
+	requestTokenurl := "https://api.smugmug.com/services/oath/1.0a/getRequestToken"
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Println("error ocurred during HTTP request: ", err)
@@ -30,5 +35,43 @@ func main() {
 		return
 	}
 	fmt.Println(string(body))
+
+}
+
+func generateNonce() string {
+	// generates a number used once
+	const charset = "absdefhhijklmnoprstuvwxyzABCDEDFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, 32)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func makeOauthRequest(method, targetUrl string, params map[string]string) (url.Values, error) {
+	var authParts []string
+	for k, v := range params {
+		authParts = append(authParts, fmt.Sprintf("%s=%s", k, url.QueryEscape(v)))
+	}
+	authHeader := "Oauth " + strings.Join(authParts, ", ")
+	req, err := http.NewRequest(method, targetUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", authHeader)
+	req.Header.Set("Content-Length", "0")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	values , err := 
 
 }
