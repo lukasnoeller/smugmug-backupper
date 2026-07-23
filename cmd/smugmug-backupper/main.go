52a690c74d22dc66e46321803feaa73dfa533d82
Lukas <lukasnoller@gmail.com>
2026-07-23 22:04:40 +0200

    Begin file/node retrieval logic

--- cmd/smugmug-backupper/main.go
+++ cmd/smugmug-backupper/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
-	"path"
-
 	"github.com/lukasnoeller/smugmug-backupper/internal/user"
 )
 
 func main() {
-	authUser := user.GetUser()
-	fmt.Println("authUser: ", authUser)
-	node := path.Base(authUser.Uris.Node.Uri)
-	fmt.Println("node", node)
+	fmt.Println("User: ", user.GetUser())
+	node := user.GetRootNode()
+	user.GetChildNodes(node)
+	//user.GetEverything()
 
 }
--- internal/user/user.go
+++ internal/user/user.go
@@ -1,19 +1,25 @@
 package user
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/lukasnoeller/smugmug-backupper/internal/auth"
+	"path"
 )
 
 type User struct {
 	Name     string `json:"Name"`
 	NickName string `json:"NickName"`
 	Uris     struct {
-		Node struct {
+		Node   Node `json:"Node"`
+		Folder struct {
 			Uri string `json:"Uri"`
-		} `json:"Node"`
+		} `json:"Folder"`
 	} `json:"Uris"`
+}
+type Node struct {
+	Uri string `json:"Uri"`
 }
 type AuthUserResponse struct {
 	Response struct {
@@ -25,10 +31,42 @@ func GetUser() User {
 
 	body, err := auth.AuthenticatedRequest("GET", "https://api.smugmug.com/api/v2!authuser")
 	var authUserResponse AuthUserResponse
-	err = json.Unmarshal([]byte(body), &authUserResponse)
+
+	err = auth.Parse(body, &authUserResponse)
+
 	if err != nil {
-		fmt.Println("error ocurred during unmarshalling of response body: ", err)
+		fmt.Println("error ocurred during unmashalling of user ", err)
 	}
 	authUser := authUserResponse.Response.User
 	return authUser
+}
+func GetRootNode() string {
+	return path.Base(GetUser().Uris.Node.Uri)
+}
+func GetChildNodes(nodeUri string) {
+	nodeRequestUrl := fmt.Sprintf("https://api.smugmug.com/api/v2/node/%s!children", nodeUri)
+	body, err := auth.AuthenticatedRequest("GET", nodeRequestUrl)
+	if err != nil {
+		fmt.Println("error ocurred during request: ", err)
+	}
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, body, "", "  "); err != nil {
+		fmt.Println("Invalid JSON:", err)
+		return
+	}
+	fmt.Println(prettyJSON.String())
+
+}
+func GetEverything() {
+	body, err := auth.AuthenticatedRequest("GET", "https://api.smugmug.com/api/v2!authuser")
+	if err != nil {
+		fmt.Println("error ocurred during request: ", err)
+	}
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, body, "", "  "); err != nil {
+		fmt.Println("Invalid JSON:", err)
+		return
+	}
+
+	fmt.Println(prettyJSON.String())
 }
